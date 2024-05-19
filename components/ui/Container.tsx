9c5fb0971a87f60ae3d7eef1c284e621b596f0eb
Timmi <tima53419@gmail.com>
2024-05-19 15:20:53 +0700

    Пофиксил анимацию появления FAQ и уменьшил паддинги на мобилках

--- components/ui/Container.tsx
+++ components/ui/Container.tsx
@@ -8,7 +8,7 @@ interface ContainerProps {
 export const Container = ({ children, gap }: ContainerProps) => {
 	return (
 		<div
-			className={`flex flex-col max-w-[1300px] mx-auto px-[40px] my-[60px]`}
+			className={`flex flex-col max-w-[1300px] mx-auto xs:px-[10px] sm:px-[10px] 2xl:px-[40px] my-[60px]`}
 			style={{ gap: gap || '60px' }}
 		>
 			{children}
